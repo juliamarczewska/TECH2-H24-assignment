4d86660458926b42c55622a13083b5dc68b433a0
juliamarczewska <julia.marczewska@student.nhh.no>
2024-10-02 12:00:09 +0200

    Update part_A.py

--- part_A.py
+++ part_A.py
@@ -5,6 +5,34 @@ Write the implementation of part A of the exercise below.
 """
 
 def std_loops(x):
+
+        x = [1, 2, 3, 4, 5]
+
+    n = len(x)
+    mean = 0
+
+    
+    for value in x:
+        mean += value
+    mean /= n
+
+    vsum = 0
+    
+    
+    for value in x:
+        vsum += (value - mean) ** 2
+        
+    variance = vsum / n
+    standard_deviation = variance ** 0.5
+    
+    return standard_deviation
+
+
+result = std_loops([1, 2, 3, 4, 5])
+print(f'The standard deviation equals {result:.10f}') 
+ 
+    
+
     """
     Compute standard deviation of x using loops.
 
