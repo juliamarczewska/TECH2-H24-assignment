9de260dbebff35a5d11c16657cd61749193d2723
juliamarczewska <julia.marczewska@student.nhh.no>
2024-10-05 12:50:18 +0200

    Final part_A.py

--- part_A.py
+++ part_A.py
@@ -5,64 +5,38 @@ Write the implementation of part A of the exercise below.
 """
 
 #using loops function 
+
+from math import sqrt
+
 def std_loops(x):
-
-    number = len(x)
-
-    total_sum = 0
+    N = len(x)
+    mean = sum(x) / N
+    sum_squares = sum([(i - mean) ** 2 for i in x])
+    variance = sum_squares / N
+    return sqrt(variance)
     
-    for value in x:
-        total_sum += value
-    mean = total_sum / number
- 
-    vsum = 0
-    for value in x:
-        vsum += (value - mean) ** 2
-    
-    variance = vsum / number
-    standard_deviation = variance ** 0.5
-    
-    return standard_deviation
-
-result_loops = std_loops([1, 2, 3, 4, 5])
-
 #using builtin function 
     
-import math
-from math import sqrt
 def std_builtin(x):
-
-        x = [1, 2, 3, 4, 5]
-        mean = sum(x) / len(x)
-        variance = sum((xi - mean) ** 2 for xi in x) / len(x) 
-
-        standart_deviation = math.sqrt(variance)
-
-        return standart_deviation
-        
-result_builtin = std_builtin([1, 2, 3, 4, 5])
-
+    N = len(x)
+    mean = sum(x) / N
+    mean_of_squares = sum([i**2 for i in x]) / N
+    variance = mean_of_squares - (mean ** 2)
+    return sqrt(variance)
+    
 #using std() from numpy 
 
 import numpy as np
 
-x = np.array([1, 2, 3, 4, 5])
-
-result_numpy = np.std(x)
-  
-
+def std_numpy(x):
+    return np.std(x)
+    
 #checking if loops, builtin functions, and std() from numpy calculate the same result
 
 x = [1, 2, 3, 4, 5]
 
-
-result_loops = std_loops(x)
-result_builtin = std_builtin(x)
-result_numpy = np.std(x)
-
-print(f'Standard Deviation (loops): {result_loops:.10f}')
-print(f'Standard Deviation (builtin): {result_builtin:.10f}')
-print(f'Standard Deviation (std() from NumPy): {result_numpy:.10f}')
-
+print("Standard Deviation using loops:", std_loops(x))
+print("Standard Deviation using built-in functions:", std_builtin(x))
+print("Standard Deviation using NumPy:", std_numpy(x))
 
     
